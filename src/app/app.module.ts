5dff14c9afad5b0ee97afafecccd53d1844d3a04
eduardo.henrique@livetouch.com.br <*Dota27684*>
2022-02-11 11:56:45 -0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -18,7 +22,14 @@ import { ExtratoComponent } from './extrato/extrato.component';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -11,5 +11,7 @@ export class ExtratoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() transferencias: any[];
 
 }
